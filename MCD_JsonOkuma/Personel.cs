69c55be556962fd6eae6c8386fb2ef8febf199af
jackup <yk.58@hotmail.com>
2021-12-22 21:18:27 +0300

    Add project files.

---
+++ MCD_JsonOkuma/Personel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_JsonOkuma
+{
+    class Personel
+    {
+        public Guid ID { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string email { get; set; }
+        public string telefon { get; set; }
+        public string sehir { get; set; }
+
+        public override string ToString()
+        {
+            return isim + " " + soyisim;
+        }
+    }
+}
---
+++ MCD_JsonOkuma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MCD_JsonOkuma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonOkunanData = File.ReadAllText("c:\\Jsonİşlemlerim\\Personellerim.json");
+            List<Personel> data = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Personel>>(jsonOkunanData);
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+        }
+    }
+}
